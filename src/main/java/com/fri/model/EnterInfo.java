a7fd1f6113bc85dd5f5d32d086139c0dc6f59695
yangsong <ys10194@163.com>
2020-09-03 13:51:59 +0800

    境内录入 入库

---
+++ src/main/java/com/fri/model/EnterInfo.java
@@ -0,0 +1,393 @@
+package com.fri.model;
+
+public class EnterInfo {
+    private Long id;
+
+    private Integer age;
+
+    private String brunchDeptName;
+
+    private String brunchDeptNo;
+
+    private String cardType;
+
+    private String cardTypeCn;
+
+    private String checkObject;
+
+    private String checkResult;
+
+    private String checkTask;
+
+    private String disposalWay;
+
+    private String guoJi;
+
+    private String guoJiCn;
+
+    private String houseHolds;
+
+    private String identify;
+
+    private String location;
+
+    private String locationName;
+
+    private String locationNameLevelOne;
+
+    private String locationNameLevelTwo;
+
+    private String locationNameReal;
+
+    private String minzu;
+
+    private String minzuCn;
+
+    private String name;
+
+    private String personInfoJson;
+
+    private String policeDeptName;
+
+    private String policeDeptNo;
+
+    private String policeIdCard;
+
+    private String policeNo;
+
+    private String sex;
+
+    private String sexCn;
+
+    private String state;
+
+    private String stationName;
+
+    private String stationNo;
+
+    private Boolean together;
+
+    private String updateUser;
+
+    private String warningInfoDetail;
+
+    private String warningInfoShortHands;
+
+    private String xzqh;
+
+    private String xzqhCn;
+
+    private String checkinfoid;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getBrunchDeptName() {
+        return brunchDeptName;
+    }
+
+    public void setBrunchDeptName(String brunchDeptName) {
+        this.brunchDeptName = brunchDeptName;
+    }
+
+    public String getBrunchDeptNo() {
+        return brunchDeptNo;
+    }
+
+    public void setBrunchDeptNo(String brunchDeptNo) {
+        this.brunchDeptNo = brunchDeptNo;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+    public String getCardTypeCn() {
+        return cardTypeCn;
+    }
+
+    public void setCardTypeCn(String cardTypeCn) {
+        this.cardTypeCn = cardTypeCn;
+    }
+
+    public String getCheckObject() {
+        return checkObject;
+    }
+
+    public void setCheckObject(String checkObject) {
+        this.checkObject = checkObject;
+    }
+
+    public String getCheckResult() {
+        return checkResult;
+    }
+
+    public void setCheckResult(String checkResult) {
+        this.checkResult = checkResult;
+    }
+
+    public String getCheckTask() {
+        return checkTask;
+    }
+
+    public void setCheckTask(String checkTask) {
+        this.checkTask = checkTask;
+    }
+
+    public String getDisposalWay() {
+        return disposalWay;
+    }
+
+    public void setDisposalWay(String disposalWay) {
+        this.disposalWay = disposalWay;
+    }
+
+    public String getGuoJi() {
+        return guoJi;
+    }
+
+    public void setGuoJi(String guoJi) {
+        this.guoJi = guoJi;
+    }
+
+    public String getGuoJiCn() {
+        return guoJiCn;
+    }
+
+    public void setGuoJiCn(String guoJiCn) {
+        this.guoJiCn = guoJiCn;
+    }
+
+    public String getHouseHolds() {
+        return houseHolds;
+    }
+
+    public void setHouseHolds(String houseHolds) {
+        this.houseHolds = houseHolds;
+    }
+
+    public String getIdentify() {
+        return identify;
+    }
+
+    public void setIdentify(String identify) {
+        this.identify = identify;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public String getLocationNameLevelOne() {
+        return locationNameLevelOne;
+    }
+
+    public void setLocationNameLevelOne(String locationNameLevelOne) {
+        this.locationNameLevelOne = locationNameLevelOne;
+    }
+
+    public String getLocationNameLevelTwo() {
+        return locationNameLevelTwo;
+    }
+
+    public void setLocationNameLevelTwo(String locationNameLevelTwo) {
+        this.locationNameLevelTwo = locationNameLevelTwo;
+    }
+
+    public String getLocationNameReal() {
+        return locationNameReal;
+    }
+
+    public void setLocationNameReal(String locationNameReal) {
+        this.locationNameReal = locationNameReal;
+    }
+
+    public String getMinzu() {
+        return minzu;
+    }
+
+    public void setMinzu(String minzu) {
+        this.minzu = minzu;
+    }
+
+    public String getMinzuCn() {
+        return minzuCn;
+    }
+
+    public void setMinzuCn(String minzuCn) {
+        this.minzuCn = minzuCn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPersonInfoJson() {
+        return personInfoJson;
+    }
+
+    public void setPersonInfoJson(String personInfoJson) {
+        this.personInfoJson = personInfoJson;
+    }
+
+    public String getPoliceDeptName() {
+        return policeDeptName;
+    }
+
+    public void setPoliceDeptName(String policeDeptName) {
+        this.policeDeptName = policeDeptName;
+    }
+
+    public String getPoliceDeptNo() {
+        return policeDeptNo;
+    }
+
+    public void setPoliceDeptNo(String policeDeptNo) {
+        this.policeDeptNo = policeDeptNo;
+    }
+
+    public String getPoliceIdCard() {
+        return policeIdCard;
+    }
+
+    public void setPoliceIdCard(String policeIdCard) {
+        this.policeIdCard = policeIdCard;
+    }
+
+    public String getPoliceNo() {
+        return policeNo;
+    }
+
+    public void setPoliceNo(String policeNo) {
+        this.policeNo = policeNo;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getSexCn() {
+        return sexCn;
+    }
+
+    public void setSexCn(String sexCn) {
+        this.sexCn = sexCn;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public void setStationName(String stationName) {
+        this.stationName = stationName;
+    }
+
+    public String getStationNo() {
+        return stationNo;
+    }
+
+    public void setStationNo(String stationNo) {
+        this.stationNo = stationNo;
+    }
+
+    public Boolean getTogether() {
+        return together;
+    }
+
+    public void setTogether(Boolean together) {
+        this.together = together;
+    }
+
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public String getWarningInfoDetail() {
+        return warningInfoDetail;
+    }
+
+    public void setWarningInfoDetail(String warningInfoDetail) {
+        this.warningInfoDetail = warningInfoDetail;
+    }
+
+    public String getWarningInfoShortHands() {
+        return warningInfoShortHands;
+    }
+
+    public void setWarningInfoShortHands(String warningInfoShortHands) {
+        this.warningInfoShortHands = warningInfoShortHands;
+    }
+
+    public String getXzqh() {
+        return xzqh;
+    }
+
+    public void setXzqh(String xzqh) {
+        this.xzqh = xzqh;
+    }
+
+    public String getXzqhCn() {
+        return xzqhCn;
+    }
+
+    public void setXzqhCn(String xzqhCn) {
+        this.xzqhCn = xzqhCn;
+    }
+
+    public String getCheckinfoid() {
+        return checkinfoid;
+    }
+
+    public void setCheckinfoid(String checkinfoid) {
+        this.checkinfoid = checkinfoid;
+    }
+}
